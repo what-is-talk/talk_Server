600b7d989e56180d638c243f319599b01675a531
morenow <jjang9877@gmail.com>
2023-02-08 13:41:24 +0900

    Docs: change setting of application.properties

--- src/main/java/com/talk/domain/ChatRoom.java
+++ src/main/java/com/talk/domain/ChatRoom.java
@@ -6,9 +6,7 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 @Data
