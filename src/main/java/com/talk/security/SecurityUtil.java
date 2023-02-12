621ecd6be34678d3c2f26eafcfdbd7b89d20b387
morenow <jjang9877@gmail.com>
2023-02-13 00:53:54 +0900

    Fix: add .gitignore

--- src/main/java/com/talk/security/SecurityUtil.java
+++ src/main/java/com/talk/security/SecurityUtil.java
@@ -4,12 +4,6 @@ import com.talk.enums.AuthProvider;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import com.talk.domain.enumpack.AuthProvider;
-import io.jsonwebtoken.*;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
